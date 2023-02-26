89c0e3d2f3513a65c5ff2fa52a9832ba7c8ee66e
dev-ozan <111126102+dev-ozan@users.noreply.github.com>
2023-02-26 09:39:44 +0300

    Add project files.

---
+++ switchCaseWfa/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace switchCaseWfa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            monthText = new TextBox();
+            sonucLabel = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // monthText
+            // 
+            monthText.Location = new Point(26, 82);
+            monthText.Margin = new Padding(4, 4, 4, 4);
+            monthText.Name = "monthText";
+            monthText.Size = new Size(215, 29);
+            monthText.TabIndex = 0;
+            // 
+            // sonucLabel
+            // 
+            sonucLabel.BackColor = Color.Silver;
+            sonucLabel.Location = new Point(26, 143);
+            sonucLabel.Margin = new Padding(4, 0, 4, 0);
+            sonucLabel.Name = "sonucLabel";
+            sonucLabel.Size = new Size(215, 32);
+            sonucLabel.TabIndex = 1;
+            sonucLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BackColor = SystemColors.ButtonShadow;
+            label2.Location = new Point(26, 22);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(215, 32);
+            label2.TabIndex = 2;
+            label2.Text = "Months ";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(26, 210);
+            button1.Name = "button1";
+            button1.Size = new Size(215, 33);
+            button1.TabIndex = 3;
+            button1.Text = "Find The Month";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(289, 294);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(sonucLabel);
+            Controls.Add(monthText);
+            Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox monthText;
+        private Label sonucLabel;
+        private Label label2;
+        private Button button1;
+    }
+}
---
+++ switchCaseWfa/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq.Expressions;
+
+namespace switchCaseWfa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            int aylar = Convert.ToInt32(monthText.Text);
+
+            switch (aylar)
+            {
+                case 1: sonucLabel.Text = "Ocak".ToString();
+
+                    break;
+                case 2:
+                    sonucLabel.Text = "Şubat".ToString();
+
+                    break;
+
+                case 3:
+                    sonucLabel.Text = "Mart".ToString();
+
+                    break;
+
+                case 4:
+                    sonucLabel.Text = "Nisan".ToString();
+
+                    break;
+
+                case 5:
+                    sonucLabel.Text = "Mayıs".ToString();
+
+                    break;
+
+                case 6:
+                    sonucLabel.Text = "Haziran".ToString();
+
+                    break;
+
+                case 7:
+                    sonucLabel.Text = "Temmuz".ToString();
+
+                    break;
+
+                case 8:
+                    sonucLabel.Text = "Ağustos".ToString();
+
+                    break;
+                case 9:
+                    sonucLabel.Text = "Eylül".ToString();
+
+                    break;
+                case 10:
+                    sonucLabel.Text = "Ekim".ToString();
+
+                    break;
+                case 11:
+                    sonucLabel.Text = "Kasım".ToString();
+
+                    break;
+                case 12:
+                    sonucLabel.Text = "Aralık".ToString();
+
+                    break;
+
+                default:
+                    MessageBox.Show("1 ile 12 arasında değer giriniz");
+                    break;
+                        
+
+
+
+            }
+
+
+        }
+    }
+}
